817a3b744e122b64b50da019b3335dad338c65d4
Nat Williams <nat.williams@avant.com>
2018-12-04 13:24:06 -0600

    day 4 part 2

--- 2018/4.sql
+++ 2018/4.sql
@@ -5,13 +5,39 @@
 
 
 with
-  minutes as (select guard_id, date_part('minute', generate_series(lower(during), upper(during) - interval '1 minute', '1 minute')) as minute from naps),
+  minutes as (select guard_id,
+                     date_part('minute',
+                               generate_series(lower(during),
+                                               upper(during) - interval '1 minute',
+                                              '1 minute')) as minute
+              from naps),
   minutes_per_guard as (select guard_id, count(minute) from minutes group by guard_id),
   sleepiest_guard as (select guard_id from minutes_per_guard order by count desc limit 1)
 select minute, (minute * (select guard_id from sleepiest_guard)), count(minute)
 from minutes
 where guard_id = (select * from sleepiest_guard)
 group by minute
+order by count desc
+limit 20
+;
+
+
+
+/* part 2: which guard is most frequently asleep on the same minute */
+
+with
+  minutes as (select guard_id,
+                     date_part('minute',
+                               generate_series(lower(during),
+                                               upper(during) - interval '1 minute',
+                                              '1 minute')) as minute
+              from naps)
+select
+  guard_id,
+  minute,
+  count(minute)
+from minutes
+group by guard_id, minute
 order by count desc
 limit 20
 ;
