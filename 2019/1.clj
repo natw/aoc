ff25f0602091eda5531d99e68899c109a84d45ec
Nat Williams <nat.williams@amount.com>
2019-12-09 22:18:28 -0600

    day 1

--- 2019/1.clj
+++ 2019/1.clj
@@ -16,3 +16,18 @@
 (fuel-required 100756)
 
 (reduce + (map fuel-required input-lines))
+
+(defn fuel-required'
+  ([mass] (fuel-required' 0 mass))
+  ([total mass]
+   (let [req (fuel-required mass)]
+     (if (<= req 0)
+       total
+       (fuel-required' (+ total req) req))
+     )
+   )
+  )
+
+(fuel-required' 14)
+
+(reduce + (map fuel-required' input-lines))
