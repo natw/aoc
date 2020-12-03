828fe59830e0dddb565e52c967b2ab00ac1a4eb9
Nat Williams <nat.williams@amount.com>
2020-12-03 00:11:52 -0600

    got part 1

--- 2020/3.go
+++ 2020/3.go
@@ -3,25 +3,31 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
 
+var ex1 = `..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#
+`
+
 func main() {
 	f, _ := os.Open("inputs/3.txt")
 	defer f.Close()
-	s := bufio.NewScanner(f)
-	var lines []string
 
-	for s.Scan() {
-		line := s.Text()
-		lines = append(lines, line)
-	}
+	// f := strings.NewReader(ex1)
 
-	width := len(lines[0])
-	height := len(lines)
-
-	theMap := strings.Join(lines, "")
+	theMap, height, width := buildMap(f)
 
 	c := treeCount(theMap, width, height, 3, 1)
 
@@ -29,14 +35,30 @@ func main() {
 
 }
 
+func buildMap(r io.Reader) (theMap string, height int, width int) {
+	s := bufio.NewScanner(r)
+	var lines []string
+	for s.Scan() {
+		line := s.Text()
+		lines = append(lines, line)
+	}
+	width = len(lines[0])
+	height = len(lines)
+	theMap = strings.Join(lines, "")
+	return
+}
+
 func treeCount(theMap string, width int, height int, over int, down int) int {
 	count := 0
 	x := over
 	for y := down; y < height; y += down {
-		char := theMap[x%width+(width*y)]
+		index := x%width + (width * y)
+		char := theMap[index]
+		fmt.Printf("at x=%d y=%d index=%d char=%c\n", x, y, index, char)
 		if char == '#' {
 			count++
 		}
+		x += over
 	}
 	return count
 }
