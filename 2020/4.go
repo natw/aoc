f6f3fbb561ddc4f810fc08ad76f10cf3d83cb5fe
Nat Williams <nat.williams@amount.com>
2020-12-04 09:09:36 -0600

    2020 day 4 part 2

--- 2020/4.go
+++ 2020/4.go
@@ -6,11 +6,12 @@ import (
 	"io"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	part1()
+	part2()
 }
 
 func part1() {
@@ -84,4 +85,134 @@ func getPassports(f io.Reader) []string {
 	pport := strings.Join(pplines, " ")
 	pports = append(pports, pport)
 	return pports
+}
+
+//--------
+
+var ex21 = `eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007`
+
+var ex22 = `pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719`
+
+func part2() {
+	f, _ := os.Open("inputs/4.txt")
+	// f := strings.NewReader(ex22)
+
+	r := regexp.MustCompile(`(?P<key>\w{3}): ?(?P<value>\S+)`)
+	pportLines := getPassports(f)
+	valid := 0
+	for _, line := range pportLines {
+		pport := MapLine(line, r)
+		if isValid2(pport) {
+			valid++
+		}
+	}
+	fmt.Println(valid)
+}
+
+func isValid2(pport map[string]string) bool {
+	// fmt.Printf("checking: %+v\n", pport)
+	byr, err := strconv.ParseInt(pport["byr"], 10, 64)
+	if err != nil {
+		// fmt.Printf("%s gave error: %v\n", pport["byr"], err)
+		return false
+	}
+	if byr < 1920 || byr > 2002 {
+		return false
+	}
+
+	iyr, err := strconv.ParseInt(pport["iyr"], 10, 64)
+	if err != nil {
+		// fmt.Printf("%s gave error: %v\n", pport["iyr"], err)
+		return false
+	}
+	if iyr < 2010 || iyr > 2020 {
+		return false
+	}
+
+	eyr, err := strconv.ParseInt(pport["eyr"], 10, 64)
+	if err != nil {
+		return false
+	}
+
+	if eyr < 2020 || eyr > 2030 {
+		return false
+	}
+
+	height := pport["hgt"]
+	if strings.Contains(height, "cm") {
+		h, err := strconv.ParseInt(height[:len(height)-2], 10, 64)
+		// fmt.Printf("height: %s parsed: %d\n", height, h)
+		if err != nil {
+			return false
+		}
+		if h < 150 || h > 193 {
+			return false
+		}
+	} else if strings.Contains(height, "in") {
+		h, err := strconv.ParseInt(height[:len(height)-2], 10, 64)
+		// fmt.Printf("height: %s parsed inches: %d\n", height, h)
+		if err != nil {
+			return false
+		}
+		if h < 59 || h > 76 {
+			return false
+		}
+	} else {
+		return false
+	}
+
+	hclR := regexp.MustCompile(`#([0-9]|[a-f]){6}`)
+	if !hclR.Match([]byte(pport["hcl"])) {
+		// fmt.Println(pport)
+		// fmt.Printf("%s is not a color\n", pport["hcl"])
+		return false
+	}
+	// fmt.Printf("%s is a color\n", pport["hcl"])
+
+	ec := pport["ecl"]
+	if !(ec == "amb" || ec == "blu" || ec == "brn" || ec == "gry" || ec == "grn" || ec == "hzl" || ec == "oth") {
+		return false
+	}
+
+	pidR := regexp.MustCompile(`^\d{9}$`)
+	if !pidR.Match([]byte(pport["pid"])) {
+		// fmt.Printf("%s is not 9 digits\n", pport["pid"])
+		return false
+	}
+	fmt.Printf("%s is 9 digits\n", pport["pid"])
+	// fmt.Printf("checked: %+v\n", pport)
+
+	return true
+}
+
+func MapLine(line string, r *regexp.Regexp) map[string]string {
+	matches := r.FindAllStringSubmatch(line, -1)
+	p := make(map[string]string)
+	for _, m := range matches {
+		p[m[1]] = m[2]
+	}
+	return p
 }
