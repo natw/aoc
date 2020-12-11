07bf1591faa839f46a7d106661df12d754fb31d7
Nat Williams <nat.williams@amount.com>
2020-12-10 20:35:01 -0600

    2020 day 10 part 2 - wip
    after many failed attempts, I tried brute force.
    even just the second example took too long.
    
    but it was fun to write

--- 2020/10.go
+++ 2020/10.go
@@ -4,16 +4,57 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"os"
 	"sort"
 	"strconv"
+	"strings"
+	"sync"
 )
 
 func main() {
-	f, _ := os.Open("inputs/10.txt")
-	// f := strings.NewReader(ex1)
-	part1(f)
+	// f, _ := os.Open("inputs/10.txt")
+	// part1(f)
+
+	f1 := strings.NewReader(ex1) // 8
+	part2(f1)
+
+	f2 := strings.NewReader(ex2) // 19208
+	part2(f2)
+
 }
+
+// [0 1 2 3 4 7 8 9 10 11 14 17 18 19 20 23 24 25 28 31 32 33 34 35 38 39 42 45 46 47 48 49 52]
+var ex2 = `28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3
+`
 
 var ex1 = `16
 10
@@ -27,6 +68,103 @@ var ex1 = `16
 12
 4
 `
+
+func part2(f io.Reader) {
+	nums := getNums(f)
+	sort.Ints(nums)
+
+	nums = append([]int{0}, nums...)
+	max := Max(nums)
+	nums = append(nums, max+3)
+
+	fmt.Printf("%+v\n", nums)
+
+	w := work2(nums)
+	fmt.Println(w)
+
+}
+
+// 1 2 3 4 5
+func work2(nums []int) int {
+	total := 0
+
+	jobs := make(chan uint64)
+	results := make(chan bool)
+
+	workerCount := 30
+
+	go func() {
+		var i uint64
+		for i = (1 << (len(nums) - 1)) + 1; i < 1<<(len(nums)); i += 2 {
+			jobs <- i
+		}
+		close(jobs)
+	}()
+
+	var wg sync.WaitGroup
+
+	for w := 1; w <= workerCount; w++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for n := range jobs {
+				results <- worker(nums, n)
+			}
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	for v := range results {
+		if v == true {
+			total++
+		}
+	}
+
+	return total
+}
+
+func worker(nums []int, choice uint64) bool {
+	var n2 []int
+	c2 := strconv.FormatUint(choice, 2)
+	for i := range nums {
+		if c2[i] == '1' {
+			n2 = append(n2, nums[i])
+		}
+	}
+	v := isValid(n2)
+	return v
+}
+
+func isValid(nums []int) bool {
+	for i := range nums {
+		if i == 0 {
+			continue
+		}
+		if nums[i]-nums[i-1] > 3 {
+			return false
+		}
+	}
+	return true
+}
+
+func without(i int, nums []int) []int {
+	x := nums[i+1:]
+	return append(nums[:i], x...)
+}
+
+func Max(nums []int) int {
+	max := nums[0]
+	for _, n := range nums {
+		if n > max {
+			max = n
+		}
+	}
+	return max
+}
 
 func part1(f io.Reader) {
 	nums := getNums(f)
