96c1e3886498f76abfa254ba7b68760603ed28e0
Nat Williams <nat.williams@amount.com>
2020-12-14 09:14:09 -0600

    WIP 2020 day 13 part 2

--- 2020/13.go
+++ 2020/13.go
@@ -12,6 +12,12 @@ const inService = "19,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,743,x,x,x,x,x,x,x,x,x
 const ex11 = 939
 const ex12 = "7,13,x,x,59,x,31,19"
 
+// The earliest timestamp that matches the list 17,x,13,19 is 3417.
+// 67,7,59,61 first occurs at timestamp 754018.
+// 67,x,7,59,61 first occurs at timestamp 779210.
+// 67,7,x,59,61 first occurs at timestamp 1261476.
+// 1789,37,47,1889 first occurs at timestamp 1202161486.
+
 func main() {
 	// part1(ex11, splitService(ex12)) // 295
 	// part1(earliest, splitService(inService))
@@ -42,6 +48,8 @@ func splitService(line string) (ids []int) {
 }
 
 func part2(ids []int) {
+	max := Max(ids)
+
 	for i := 1; i >= 0; i++ {
 		n := i * ids[0]
 		if foundIt(n, ids) {
@@ -75,4 +83,14 @@ func part1(earliest int, inService []int) {
 		}
 		t++
 	}
+}
+
+func Max(nums []int) int {
+	max := nums[0]
+	for _, n := range nums {
+		if n > max {
+			max = n
+		}
+	}
+	return max
 }
