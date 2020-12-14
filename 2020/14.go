753971265f46885c6ecdf986829f5d07fd78cbb8
Nat Williams <nat.williams@amount.com>
2020-12-14 14:27:37 -0600

    wip examples works

--- 2020/14.go
+++ 2020/14.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"os"
 	"regexp"
 	"strconv"
 	"strings"
@@ -15,11 +14,17 @@ mem[8] = 11
 mem[7] = 101
 mem[8] = 0
 `
+var ex2 = `mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1
+`
 
 func main() {
-	part1(strings.NewReader(ex1))
-	f, _ := os.Open("inputs/14.txt")
-	part1(f)
+	// part1(strings.NewReader(ex1))
+	// f, _ := os.Open("inputs/14.txt")
+	// part1(f)
+	part2(strings.NewReader(ex2))
 }
 
 func part1(f io.Reader) {
@@ -63,6 +68,95 @@ func part1(f io.Reader) {
 		total += v
 	}
 	fmt.Println(total)
+}
+
+func part2(f io.Reader) {
+	re := regexp.MustCompile(`\d+`)
+	var mask string
+	memory := make(map[int64]int64)
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		line := s.Text()
+		if strings.Contains(line, "mask") {
+			parts := strings.Split(line, " ")
+			mask = parts[2]
+		} else {
+			matches := re.FindAllStringSubmatch(line, -1)
+			locString := matches[0][0]
+			dataDec, _ := strconv.ParseInt(matches[1][0], 10, 64)
+			locs := generateLocations(mask, locString)
+			fmt.Printf("data: %d\n", dataDec)
+			for _, loc := range locs {
+				fmt.Printf("location: %d\n", loc)
+				memory[loc] = dataDec
+			}
+		}
+	}
+
+	var total int64
+	for _, v := range memory {
+		total += v
+	}
+	fmt.Printf("total: %d\n", total)
+}
+
+// I should stop using []rune. it's []byte and those aren't actually the same
+
+func generateLocations(mask string, locDecString string) []int64 {
+	l, _ := strconv.ParseInt(locDecString, 10, 64)
+	locString := fmt.Sprintf("%36.36b", l)
+
+	locs := []int64{}
+
+	exes := []int{}
+
+	fmt.Printf("raw location: %s\n", locString)
+
+	semiMasked := make([]byte, 36, 36)
+	for i, c := range mask {
+		switch c {
+		case 'X':
+			semiMasked[i] = 'X'
+		case '1':
+			semiMasked[i] = '1'
+		case '0':
+			semiMasked[i] = locString[i]
+		}
+	}
+
+	fmt.Printf("semi masked: %c\n", semiMasked)
+
+	for i, c := range mask {
+		if c == 'X' {
+			exes = append(exes, i)
+		}
+	}
+
+	fmt.Printf("X at locations: %+v\n", exes)
+
+	possibilities := []string{}
+	for i := 1; i <= 1<<len(exes)-1; i++ {
+		possibilities = append(possibilities, fmt.Sprintf("%*.*b", len(exes), len(exes), i))
+	}
+
+	fmt.Println("location masks:")
+	fmt.Println(possibilities)
+
+	for _, poss := range possibilities {
+		l := []byte(semiMasked)
+		fmt.Printf("before applying loc mask: %c\n", l)
+		for i, xLoc := range exes {
+			l := []byte(semiMasked)
+			fmt.Printf("X %d at location %d, set to %c\n", i, xLoc, rune(poss[i]))
+			l[xLoc] = poss[i]
+			// fmt.Printf("loc: %+v\n", l)
+			l2, _ := strconv.ParseInt(string(l), 2, 64)
+			fmt.Printf("got location: %36.36b\n", l2)
+			locs = append(locs, l2)
+		}
+	}
+
+	return locs
 }
 
 func makeInt(bits []rune) int64 {
