168e99c87c003c714de5e2b614e74f41ff00f541
Nat Williams <nat.williams@amount.com>
2021-12-02 15:17:43 -0600

    formatting

--- 2021/02.clj
+++ 2021/02.clj
@@ -8,21 +8,18 @@
 (defn parse-line [line]
   (let [ps (string/split line #" ")
         dir (first ps)
-        amt (Integer. (second ps)) ]
+        amt (Integer. (second ps))]
     (case dir
       "forward" (list amt 0)
       "up" (list 0 (-' amt))
       "down" (list 0 amt))))
 
-(def test1
-  (map
-    parse-line
-    '("forward 5"
-             "down 5"
-             "forward 8"
-             "up 3"
-             "down 8"
-             "forward 2")))
+(def test1 (map parse-line '("forward 5"
+                             "down 5"
+                             "forward 8"
+                             "up 3"
+                             "down 8"
+                             "forward 2")))
 
 (def input1 (map parse-line (string-lines "2")))
 
@@ -32,9 +29,8 @@
     (if (empty? steps)
       (reduce * pos)
       (recur
-        (map + pos (first steps))
-        (rest steps)))))
-
+       (map + pos (first steps))
+       (rest steps)))))
 
 (part1 test1) ; 150
 (part1 input1) ; 2322630
@@ -43,18 +39,19 @@
 
 (defn move [[x depth aim] [forward dy]]
   (list
-    (+ x forward)
-    (+ depth (* aim forward))
-    (+ aim dy)))
+   (+ x forward)
+   (+ depth (* aim forward))
+   (+ aim dy)))
 
 (defn part2 [starting-steps]
-       (loop [pos '(0 0 0)
-              steps starting-steps]
-         (if (empty? steps)
-           (* (first pos) (second pos))
-           (recur
-             (move pos (first steps))
-             (rest steps)))))
+  (loop [pos '(0 0 0)
+         steps starting-steps]
+    (if (empty? steps)
+      (* (first pos) (second pos))
+      (recur
+       (move pos (first steps))
+       (rest steps)))))
 
 (part2 test1) ; 900
+(part2 input1) ; 2105273490
 (part2 input1) ; 2105273490
--- 2021/project.clj
+++ 2021/project.clj
@@ -4,6 +4,8 @@
   :dependencies [[org.clojure/clojure "1.10.3"]]
   :main ^:skip-aot aoc
   :target-path "target/%s"
+  :cljfmt {:remove-consecutive-blank-lines? true
+           :indents ^:replace {#"^[-*+!?_a-zA-Z]" [[:inner 0]]}}
   :plugins [[cider/cider-nrepl "0.26.0"]]
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
