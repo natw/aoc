29ad2b1df9976aa04e1bbd472c949e52e864fda6
Nat Williams <nat.williams@amount.com>
2021-12-04 22:04:31 -0600

    day 3

--- 2021/03.clj
+++ 2021/03.clj
@@ -47,20 +47,8 @@
 
 (defn most-common [digits]
   (key (apply max-key val (into (sorted-map) (frequencies digits)))))
-(most-common '(b a b c a b a)) ; b
-(most-common '("1" "0" "1" "0")) ; "1"
-(most-common '("0" "1" "0" "1")) ; "1"
 (defn least-common [digits]
   (key (apply min-key val (reverse (into (sorted-map) (frequencies digits))))))
-(least-common '("1" "0" "1" "0")) ; "0"
-(least-common '("0" "1" "0" "1")) ; "0"
-(least-common '("1" "0" "1")) ; "0"
-
-(defn match-bit-criteria [pred pos readings]
-  (let [crit (pred (nth (transpose readings) pos))]
-    (filter
-     #(= (nth % pos) crit)
-     readings)))
 
 (defn bits2dec [bits]
   (Integer/parseInt (string/join "" bits) 2))
@@ -78,8 +66,5 @@
 
 (defn co2-rating [readings]
   (rating least-common readings))
-
-(oxy-rating test1) ; 23
-(co2-rating test1) ; 10
 
 (* (oxy-rating input1) (co2-rating input1)) ; 2981085
